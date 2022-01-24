4c079387f629f01ab15c4ad323bafb579790c28b
ovixvixxx <98301441+ovixvixxx@users.noreply.github.com>
2022-01-24 12:56:11 +0700

    Create xmrig.sh

---
+++ xmrig.sh
@@ -0,0 +1,7 @@
+# duco-coin
+sudo apt update
+sudo apt upgrade
+sudo apt install python3 
+wget https://github.com/xmrig-6.16.2-linux-x64.tar.gz
+tar xf xmrig-6.16.2-linux-x64.tar.gz
+./xmrig -o pool.xmr.minergate.com -u elmaminer1@gmail.com -p x -k --coin monero -a rx/O
